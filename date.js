3784baefe47048ca3411dd55a8eb13a5f80beecb
hyewonji <60416187+hyewonji@users.noreply.github.com>
2020-08-24 23:34:00 +0900

    날짜,시간 출력

---
+++ date.js
@@ -0,0 +1,24 @@
+const today = document.querySelector(".js-clock"),
+    dateTitle = today.querySelector(".realTimeDate");
+
+function getDate() {
+    const date = new Date();
+    const y = date.getFullYear();
+    const m = date.getMonth() + 1;
+    const d = date.getDate();
+    const day = date.getDay() - 1;
+    const day2 = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+    const day3 = day2[day];
+    dateTitle.innerText = `${y}.${
+        m < 10 ? `0${m}` : m}.${
+        d < 10 ? `0${d}` : d} ${
+        day3}
+        `;
+}
+
+function init() {
+    getDate();
+    setInterval(getDate, 1000);
+}
+
+init()
