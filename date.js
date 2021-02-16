7ecf556fe82fb3cae4aa5b6f990369eb5461a004
hyewonji <60416187+hyewonji@users.noreply.github.com>
2021-02-17 01:59:44 +0900

    Modify Text Format

--- date.js
+++ date.js
@@ -9,10 +9,7 @@ function getDate() {
     const day = date.getDay() - 1;
     const day2 = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
     const day3 = day2[(day + 7) % 7];
-    dateTitle.innerText = `${y}.${
-        m < 10 ? `0${m}` : m}.${
-        d < 10 ? `0${d}` : d} ${
-        day3}
+    dateTitle.innerText = `${y}. ${m < 10 ? `0${m}` : m}. ${d < 10 ? `0${d}` : d} ${day3}
         `;
 }
 
