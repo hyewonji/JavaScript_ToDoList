0db2457fc283c5cb09a2d99a500f06b013e3ac24
hyewonji <60416187+hyewonji@users.noreply.github.com>
2021-02-26 15:37:29 +0900

    불필요한 코드 삭제, 정리
    count 배열을 새로 생성하지 않고,
    toDos의 key인 check를 사용해 "총 리스트 개수/완료된 리스트 개수" 를 나타낸다.

--- todo.js
+++ todo.js
@@ -1,46 +1,38 @@
-const form = document.querySelector(".js-toDo"),
-    input = form.querySelector("input"),
-    toDoBoard = document.querySelector(".js-toDoList");
+const toDoForm = document.querySelector(".toDoForm"),
+    input = toDoForm.querySelector("input"),
+    toDoList = document.querySelector(".toDoList");
 const toDos_LS = "toDos";
 let toDos = [];
 
 const countTitle = document.querySelector(".count");
-const counts_LS = "counts";
-let counts = [];
+let counts = 0;
 
 function saveToDos() {
     localStorage.setItem(toDos_LS, JSON.stringify(toDos));
-    countToDo();
 }
 
-function saveCounts() {
-    localStorage.setItem(counts_LS, JSON.stringify(counts));
-}
-
-function countToDo() {
-    var ele = document.getElementById('toDoList'),
-        eleCount = ele.childElementCount;
-    countTitle.innerText = `You did ${counts.length} / ${eleCount}`;
+function countToDos() {
+    countTitle.innerText = `You did ${counts} / ${toDos.length}`;
 }
 
 function delToDos(event) {
+    console.log(event.target.parentNode)
     const text = event.target.parentNode.innerText
     if (confirm(`Want to remove "${text}"?`) === true) {
         const btn = event.target;
         const li = btn.parentNode;
         const id = li.id - 1;
-        const i = li.querySelector("i");
-        if (i.className === 'fas fa-check-circle') {
-            const idx = counts.indexOf(id);
-            counts.splice(idx, 1);
-            saveCounts();
+        const checkI = li.querySelector("i");
+        if (checkI.className === 'fas fa-check-circle') {
+            counts -= 1;
         }
-        toDoBoard.removeChild(li);
+        toDoList.removeChild(li);
         const cleanToDos = toDos.filter(function (toDo) {
             return toDo.id !== parseInt(li.id);
         })
         toDos = cleanToDos;
         saveToDos();
+        countToDos();
     } else {
         return false;
     }
@@ -49,79 +41,48 @@ function delToDos(event) {
 function doneToDos(event) {
     const chk = event.target;
     const li = chk.parentNode;
-    const id = li.id;
-    const i = li.querySelector("i");
+    const id = li.id-1 ;
+    const isChecked = toDos[id].check;
+    console.log(id,isChecked,toDos)
+    const checkI = li.querySelector("i");
     const span = li.querySelector("span");
-    const text = span.innerText;
-    function isChecked(element) {
-        if (element.text === text) {
-            return true;
-        }
-    }
-    const done = toDos.find(isChecked);
-    if (done.check === 0) {
-        done.check = 1;
-        i.className = "fas fa-check-circle"
-        span.classList.add("checked");
-        counts.push(id);
+    if (isChecked) {
+        toDos[id].check = 0;
+        console.log(toDos)
+        checkI.className = "far fa-circle"
+        counts -= 1
     } else {
-        done.check = 0;
-        i.className = "far fa-circle";
-        span.classList.remove("checked");
-        const idx = counts.indexOf(id);
-        counts.splice(idx, 1);
+        toDos[id].check = 1;
+        console.log(toDos)
+        checkI.className = "fas fa-check-circle";
+        counts += 1;
     }
     saveToDos();
-    saveCounts();
+    countToDos();
 };
 
-function paintToDo(text) {
+function paintToDo(text,check) {
     const li = document.createElement("li");
     const checkBox = document.createElement("i");
-    const toDoList = document.createElement("span");
+    const toDo = document.createElement("span");
     const delBtn = document.createElement("i");
     const newId = toDos.length + 1;
-    checkBox.className = "far fa-circle";
-    toDoList.innerText = text;
+    checkBox.className = (check ? "fa-check-circle" : "far fa-circle");
+    toDo.innerText = text;
     delBtn.className = "far fa-trash-alt"
     li.appendChild(checkBox);
-    li.appendChild(toDoList);
+    li.appendChild(toDo);
     li.appendChild(delBtn);
     li.id = newId;
-    toDoBoard.appendChild(li);
+    toDoList.appendChild(li);
     const toDoObj = {
         text: text,
         id: newId,
-        check: 0
+        check: (check ? 1 : 0)
     };
     toDos.push(toDoObj);
     saveToDos();
-    delBtn.addEventListener("click", delToDos);
-    checkBox.addEventListener("click", doneToDos);
-}
-
-function paintToDoChecked(text) {
-    const li = document.createElement("li");
-    const checkBox = document.createElement("i");
-    const toDoList = document.createElement("span");
-    const delBtn = document.createElement("i");
-    const newId = toDos.length + 1;
-    checkBox.className = "fas fa-check-circle";
-    toDoList.innerText = text;
-    toDoList.classList.add("checked");
-    delBtn.className = "far fa-trash-alt";
-    li.appendChild(checkBox);
-    li.appendChild(toDoList);
-    li.appendChild(delBtn);
-    li.id = newId;
-    toDoBoard.appendChild(li);
-    const toDoObj = {
-        text: text,
-        id: newId,
-        check: 1
-    };
-    toDos.push(toDoObj);
-    saveToDos();
+    countToDos();
     delBtn.addEventListener("click", delToDos);
     checkBox.addEventListener("click", doneToDos);
 }
@@ -129,41 +90,30 @@ function paintToDoChecked(text) {
 function handleSubmit(event) {
     event.preventDefault();
     const currentValue = input.value;
-    paintToDo(currentValue);
-    countToDo();
+    paintToDo(currentValue,0);
     input.value = "";
-}
-
-function loadCounts() {
-    var ele = document.getElementById('toDoList'),
-        eleCount = ele.childElementCount;
-    const loadCounts = localStorage.getItem(counts_LS);
-    const parsedCounts = JSON.parse(loadCounts);
-    parsedCounts.forEach(function (count) {
-        counts.push(count);
-    });
-    countTitle.innerText = `You did ${parsedCounts.length} / ${eleCount}`;
 }
 
 function loadToDos() {
     const loadToDos = localStorage.getItem(toDos_LS);
     if (loadToDos !== null) {
         const parsedToDos = JSON.parse(loadToDos);
+        const ToDosNum = parsedToDos.length;
         parsedToDos.forEach(function (toDo) {
-            if (toDo.check === 1) {
-                paintToDoChecked(toDo.text);
-            } else {
-                paintToDo(toDo.text);
-            }
+            const text = toDo.text;
+            const check = toDo.check;
+            paintToDo(text,check);
+            if(check){
+                counts += 1
+            };
         });
-        loadCounts();
     }
 }
 
-
 function init() {
     loadToDos();
-    form.addEventListener("submit", handleSubmit);
+    countToDos();
+    toDoForm.addEventListener("submit", handleSubmit);
 }
 
 init();
