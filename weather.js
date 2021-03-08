145126641ab19fced0b8f331cf4b11cda4fb26af
hyewonji <60416187+hyewonji@users.noreply.github.com>
2021-03-08 15:04:49 +0900

    Get Weather API and Show Current Weather by Image

--- weather.js
+++ weather.js
@@ -1,33 +1,81 @@
+const weather = document.querySelector(".realTimeWeather");
+
 const API_KEY = 'ed3b96c06f6343ed844ef371d65d6a1b';
+const COORDS_LS = 'coords';
+
+function showWeather(weatherObj){
+    const desc = weatherObj.description;
+    const img = document.createElement("img");
+    img.className = "weatherImg";
+    if(desc === 'clear sky'){
+        img.src = "http://openweathermap.org/img/wn/01d@2x.png";
+    } else if(desc === 'few clouds'){
+        img.src = "http://openweathermap.org/img/wn/02d@2x.png";
+    } else if(desc === 'scattered clouds' && desc === 'broken clouds'){
+        img.src = "http://openweathermap.org/img/wn/03d@2x.png";
+    } else if(desc === 'shower rain' && desc === 'rain'){
+        img.src = "http://openweathermap.org/img/wn/10d@2x.png";
+    } else if(desc === 'thunderstorm'){
+        img.src = "http://openweathermap.org/img/wn/11d@2x.png";
+    } else if(desc === 'snow'){
+        img.src = "http://openweathermap.org/img/wn/13d@2x.png";
+    } else if(desc === 'mist'){
+        img.src = "http://openweathermap.org/img/wn/50d@2x.png";
+    } 
+    
+    weather.appendChild(img);
+}
 
 function getWeatherApi(lat,lon){
     fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`)
     .then(function(response){
         return response.json();
     }).then(function(json){
-        console.log(json);
-    })
-}
-
-function getGeoLocation(lat,lon){
-    navigator.geolocation.getCurrentPosition(
-        (position)=>{
-            lat = position.coords.latitude;
-            lon = position.coords.longitude;
+        const temperature = json.main.temp,
+            name = json.name,
+            description = json.weather[0].description;
+        const weatherObj = {
+            temperature,
+            name,
+            description
         }
-    )
+        showWeather(weatherObj);
+    });
 }
 
-async function getWeather(){
-    let lat, lon;
-    await getGeoLocation(lat,lon);
-    console.log(lat,lon);
-    await getWeatherApi(lat,lon);
-    console.log(2);
+function handleGeoError(e){
+    console.log(e);
+}
+
+function saveCoors(lat,lon){
+    const coordsObj = {
+        latitude:lat,
+        longitude:lon
+    }
+    localStorage.setItem(COORDS_LS,JSON.stringify(coordsObj));
+}
+
+function handleGeoSuccess(position){
+    const loadedCoords = localStorage.getItem(COORDS_LS);
+    const parseCoords = JSON.parse(loadedCoords);
+    const lat_LS = parseCoords.latitude,
+        lon_LS = parseCoords.longitude;
+
+    const latitude = position.coords.latitude,
+        longitude = position.coords.longitude;
+
+    if(loadedCoords === null || (lat_LS - latitude) > 0.1 || (lon_LS - longitude) > 0.1){
+        saveCoors(latitude,longitude);
+    }
+    getWeatherApi(lat_LS,lon_LS);
+}
+
+function getGeoLocation(){
+    navigator.geolocation.getCurrentPosition(handleGeoSuccess,handleGeoError);
 }
 
 function init(){
-    getWeather();
+    getGeoLocation();
 }
 
 init();
